c8be07ef4d7b1469d5488dbc06241e74957af6b1
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-05-02 22:44:28 -0300

    feat: Criacao de transacoes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,26 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = knex('transactions').where('amount', 1000).select('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['debit', 'credit']),
+    })
 
-    return transactions
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    reply.status(201).send()
   })
 }
