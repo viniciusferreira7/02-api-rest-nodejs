9821166e233f22da7b29a658dd9b8be08d29fba4
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-05-23 21:33:33 -0300

    test: Testando criacao de transacao

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,7 +1,22 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, test } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('should status code is 201', () => {
-  const status = 201
+beforeAll(async () => {
+  await app.ready()
+})
 
-  expect(status).toEqual(201)
+afterAll(async () => {
+  await app.close()
+})
+
+test('user can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 3500,
+      type: 'credit',
+    })
+    .expect(201)
 })
