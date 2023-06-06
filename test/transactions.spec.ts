d4bdcd8d0b8da7431401f81397d54dbfc1d5090d
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-06-06 19:05:02 -0300

    test: Finalizando testes da aplicacao

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -51,4 +51,68 @@ describe('Transactions routes', () => {
       }),
     ])
   })
+
+  it('should be able to get specific transaction ', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 3500,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    console.log(getTransactionResponse.body)
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 3500,
+      }),
+    )
+  })
+
+  it('should be able to get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Debit transaction',
+        amount: 3000,
+        type: 'debit',
+      })
+
+    const SummaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+
+    console.log(SummaryResponse.body)
+
+    // expect(SummaryResponse.body.summary).toEqual({
+    //   amount: 2000,
+    // })
+  })
 })
